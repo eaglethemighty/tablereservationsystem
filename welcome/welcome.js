2dd2922defedf26cd8aa7204803417c0414548b7
LCzunko <LCzunko@gmail.com>
2021-11-05 12:36:52 +0100

    Fix footer and document structures

--- welcome/welcome.js
+++ welcome/welcome.js
@@ -1,15 +1,11 @@
-fetch("../header/header.html")
-    .then(response => response.text())
-    .then(data => document.querySelector("header").innerHTML = data);
-
 function ClickLink(href) {
     location.href = href;
 }
+
+fetch("../header/header.html")
+    .then(response => response.text())
+    .then(data => document.getElementById('header').innerHTML = data);
 
 fetch("../footer/footer.html")
     .then(response => response.text())
-    .then(data => document.querySelector("footer").innerHTML = data);
-
-function ClickLink(href) {
-    location.href = href;
-}
+    .then(data => document.getElementById('footer').innerHTML = data);
