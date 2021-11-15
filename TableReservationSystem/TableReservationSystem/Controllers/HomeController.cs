7bba45f4374e7784b801e83ba13b40e8e063e2e6
a_michalak <mot+eaglethemighty@users.noreply.github.com>
2021-11-15 13:45:15 +0100

    Move welcome and header into views

--- TableReservationSystem/TableReservationSystem/Controllers/HomeController.cs
+++ TableReservationSystem/TableReservationSystem/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using TableReservationSystem.Models;
 
 namespace TableReservationSystem.Controllers
@@ -19,11 +15,6 @@ namespace TableReservationSystem.Controllers
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
