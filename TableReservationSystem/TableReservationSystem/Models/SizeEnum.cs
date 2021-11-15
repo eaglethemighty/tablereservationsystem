c1d0c7cfa79f2d6475558a2ce1023ddaaf2f6eee
SylwiaLeung <longdepati@gmail.com>
2021-11-15 14:36:49 +0100

    Change enum properties

--- TableReservationSystem/TableReservationSystem/Models/SizeEnum.cs
+++ TableReservationSystem/TableReservationSystem/Models/SizeEnum.cs
@@ -4,17 +4,8 @@ namespace TableReservationSystem.Models
 {
     public enum Size
     {
-        [Description("1")]
-        one,
-        [Description("2")]
-        two,
-        [Description("4")]
-        four,
-        [Description("5")]
-        five,
-        [Description("6")]
-        six,
-        [Description("12")]
-        twelve
+        Small,
+        Medium,
+        Large
     }
 }
