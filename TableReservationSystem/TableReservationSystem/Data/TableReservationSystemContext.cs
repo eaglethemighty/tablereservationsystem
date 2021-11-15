e1c8df0749669a5f0e21b356ad815a1b274b400a
SylwiaLeung <longdepati@gmail.com>
2021-11-15 13:48:30 +0100

    Create DbContext

---
+++ TableReservationSystem/TableReservationSystem/Data/TableReservationSystemContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TableReservationSystem.Models;
+
+namespace TableReservationSystem.Data
+{
+    public class TableReservationSystemContext : DbContext
+    {
+        public TableReservationSystemContext (DbContextOptions<TableReservationSystemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TableReservationSystem.Models.Table> Table { get; set; }
+
+        public DbSet<TableReservationSystem.Models.Reservation> Reservation { get; set; }
+    }
+}
