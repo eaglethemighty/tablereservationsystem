60afaeebb2cb3af4be14fc24d3957d9d7ad7d0b1
grzegorzmajmurek <61845216+grzegorzmajmurek@users.noreply.github.com>
2021-11-17 11:14:34 +0100

    add dao reservation

---
+++ TableReservationSystem/TableReservationSystem/Repositories/ReservationRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TableReservationSystem.Data;
+using TableReservationSystem.Models;
+
+namespace TableReservationSystem.Repositories
+{
+    public class ReservationRepository : IReservationRepository
+    {
+        private readonly TableReservationSystemContext _context;
+        public ReservationRepository(TableReservationSystemContext context)
+        {
+            _context = context;
+        }
+        public void Create(Reservation reservation)
+        {
+            _context.Reservation.Add(reservation);
+            _context.SaveChanges();
+        }
+
+        public List<Reservation> GetAllReservation()
+            => _context.Reservation.ToList();
+    }
+}
