2640ef288147bb7774c71d6a7f82dcf6fad99ed0
LCzunko <LCzunko@gmail.com>
2021-11-17 10:48:54 +0100

    Begin implementing ajax loading

---
+++ TableReservationSystem/TableReservationSystem/Models/AjaxData.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TableReservationSystem.Models
+{
+    public class AjaxData
+    {
+        public DateTime DateTime { get; set; }
+        public int Number { get; set; }
+    }
+}
